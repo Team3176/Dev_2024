b7866bc193cc434d8ac27e1fef1ec7de78df92ba
Malcolm Laptop <char3176.malcolm@gmail.com>
2023-12-14 08:05:04 -0500

    this is char fixing this branch step2

--- motor_mover/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ motor_mover/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
@@ -26,7 +26,7 @@ public class ExampleSubsystem extends SubsystemBase {
     falcon.set(100.0);
 
   }
-  public CommandBase mvFowardCommand() {
+  public Command mvFowardCommand() {
     // Inline construction of command goes here.
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(
@@ -36,7 +36,7 @@ public class ExampleSubsystem extends SubsystemBase {
           /* one-tme action goes here */
         });
   }
-  public CommandBase moveBackwardCommand() {
+  public Command moveBackwardCommand() {
     // Inline construction of command goes here.
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(
@@ -46,7 +46,7 @@ public class ExampleSubsystem extends SubsystemBase {
           /* one-tme action goes here */
         });
   }
-  public CommandBase stopCommand() {
+  public Command stopCommand() {
     // Stops the motor
     return runOnce(
         () -> {
