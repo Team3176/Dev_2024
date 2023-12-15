4d05adb01383caaca72afa3cf28636175236a9ee
mildred <mildred032709@gmail.com>
2023-12-14 19:41:51 -0500

    proof of skills 4. linebreak test.

--- motor_mover/src/main/java/frc/robot/RobotContainer.java
+++ motor_mover/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.linebreakCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -52,7 +53,7 @@ public class RobotContainer {
     m_driverController.x().onTrue(m_exampleSubsystem.moveBackwardsCommand());
     m_driverController.a().onTrue(m_exampleSubsystem.stopCommand());
     m_driverController.y().whileTrue(new ExampleCommand(m_exampleSubsystem));
-    
+    m_driverController.start().onTrue(new linebreakCommand(m_exampleSubsystem));
   }
 
   /**
---
+++ motor_mover/src/main/java/frc/robot/commands/linebreakCommand.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+/** An example command that uses an example subsystem. */
+public class linebreakCommand extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final ExampleSubsystem m_subsystem; 
+  // Initializes a DigitalInput on DIO 0
+  DigitalInput input = new DigitalInput(0);
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public linebreakCommand(ExampleSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_subsystem.moveForward();// Initializes a DigitalInput on DIO 0
+    DigitalInput input = new DigitalInput(9);
+    
+  }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_subsystem.stopMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(input.get() == true){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+}
