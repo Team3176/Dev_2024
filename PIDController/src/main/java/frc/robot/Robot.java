3fcb6a81b8773d6e3816358320408bca620384dc
Jazz <jazzanthonymiller@gmail.com>
2024-01-25 20:49:28 -0500

    PID SPIN!!!!!!!!!!

--- PIDController/src/main/java/frc/robot/Robot.java
+++ PIDController/src/main/java/frc/robot/Robot.java
@@ -4,23 +4,95 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.sim.PhysicsSim;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  private final TalonFX m_fx = new TalonFX(4);
+   private final PositionVoltage m_voltagePosition = new PositionVoltage(0, 0, true, 0, 0, false, false, false);
+   private final PositionTorqueCurrentFOC m_torquePosition = new PositionTorqueCurrentFOC(0, 0, 0, 1, false, false, false);
 
-  private RobotContainer m_robotContainer;
+   private final NeutralOut m_brake = new NeutralOut();
 
-  @Override
+   private final XboxController m_joystick = new XboxController(0);
+   
+ 
+   @Override
   public void robotInit() {
-    m_robotContainer = new RobotContainer();
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+    configs.Slot0.kP = 2.4; // An error of 0.5 rotations results in 1.2 volts output
+    configs.Slot0.kD = 0.1; // A change of 1 rotation per second results in 0.1 volts output
+    // Peak output of 8 volts
+    configs.Voltage.PeakForwardVoltage = 8;
+    configs.Voltage.PeakReverseVoltage = -8;
+    
+    configs.Slot1.kP = 40; // An error of 1 rotations results in 40 amps output
+    configs.Slot1.kD = 2; // A change of 1 rotation per second results in 2 amps output
+    // Peak output of 130 amps
+    configs.TorqueCurrent.PeakForwardTorqueCurrent = 130;
+    configs.TorqueCurrent.PeakReverseTorqueCurrent = 130;
+
+    /* Retry config apply up to 5 times, report if failure */
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      status = m_fx.getConfigurator().apply(configs);
+      if (status.isOK()) break;
+    }
+    if(!status.isOK()) {
+      System.out.println("Could not apply configs, error code: " + status.toString());
+    }
+
+
+    /* Make sure we start at 0 */
+    m_fx.setPosition(0);
+    
   }
 
   @Override
   public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
+    
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double desiredRotations = m_joystick.getLeftX() * 10; 
+
+    if (m_joystick.getAButtonPressed()) {
+      /* Use voltage position */
+      m_fx.setControl(m_voltagePosition.withPosition(desiredRotations));
+    }
+
+    else {
+      /* Disable the motor instead */
+      m_fx.setControl(m_brake);
+    }
+  
+   
   }
 
   @Override
@@ -30,45 +102,18 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+  public void testInit() {}
 
   @Override
   public void testPeriodic() {}
 
   @Override
-  public void testExit() {}
-  
+  public void simulationInit() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {
+   
+  }
 }
